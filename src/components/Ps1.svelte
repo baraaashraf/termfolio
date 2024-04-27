8306829fe04da2caa632393760899e80f30428d7
baraaashraf <baraaashraf44@gmail.com>
2024-04-27 21:55:07 +0800

    ps1

--- src/components/Ps1.svelte
+++ src/components/Ps1.svelte
@@ -1,10 +1,16 @@
 <script>
-  import { theme } from '../stores/theme';
+  import { fetchIPAddress } from "../utils/detectBrowser";
+  import { theme } from "../stores/theme";
   let hostname = window.location.hostname;
+  let ip = fetchIPAddress()
 </script>
 
 <h1 class="font-bold flex">
+  {#await fetchIPAddress()}
   <span style={`color: ${$theme.yellow};`}>guest</span>
+  {:then ip}
+  <span style={`color: ${$theme.yellow};`}>{ip}</span>
+  {/await}
   <span style={`color: ${$theme.white}`}>@</span>
   <span style={`color: ${$theme.green}`}>{hostname}</span>
   <span style={`color: ${$theme.white}`}>:~$</span>
