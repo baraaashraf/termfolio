5371663450e6698c60d34628b1fd973a8027f324
baraaashraf <baraaashraf44@gmail.com>
2024-04-27 21:27:54 +0800

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    target: 'esnext' //browsers can handle the latest ES features
+  },
   server: {
     port: 3000,
   },
